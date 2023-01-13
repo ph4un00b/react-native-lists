640983bc802ec31cbef3daa9e6d404d01f82271c
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-01-13 14:52:31 -0600

    chore: add another list of debug routes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import Constants from "expo-constants";
-import React from "react";
-import { Suspense } from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React, { Suspense } from "react";
+import { SafeAreaView, Text, View } from "react-native";
 import {
   GestureHandlerRootView,
   ScrollView,
@@ -14,20 +13,22 @@ import {
   useLocation,
 } from "react-router-native";
 import { $debug } from "./src/global/atoms";
-import { APP_LINKS } from "./src/models/routes";
+import { APP_LINKS, IMG_LINKS } from "./src/models/routes";
 import { useAtom } from "jotai";
 import "./styles";
 
+const LINKS = APP_LINKS.concat(IMG_LINKS);
 export default function App() {
   const [debug] = useAtom($debug);
   return (
     <GestureHandlerRootView className="flex-1">
       <NativeRouter>
         <SafeAreaView className="flex-1">
-          {debug && <BarNavigation />}
+          {debug && <BarNavigation links={APP_LINKS} />}
+          {debug && <BarNavigation links={IMG_LINKS} />}
           <Suspense>
             <Routes>
-              {APP_LINKS.map((link) => (
+              {LINKS.map((link) => (
                 <React.Fragment key={link.id}>
                   <Route path={link.path} element={link.screen} />
                 </React.Fragment>
@@ -42,7 +43,7 @@ export default function App() {
 
 type TabProps = { children: React.ReactNode; to: string };
 
-function BarNavigation() {
+function BarNavigation({ links }: { links: Record<string, any>[] }) {
   return (
     <View
       className="flex flex-row w-full bg-slate-900"
@@ -52,7 +53,7 @@ function BarNavigation() {
         horizontal
         style={{ paddingBottom: Constants.statusBarHeight * 0.5 }}
       >
-        {APP_LINKS.map((link) => (
+        {links.map((link) => (
           <React.Fragment key={link.id}>
             <AppBarTab to={link.path}>{link.alias}</AppBarTab>
           </React.Fragment>
@@ -65,10 +66,9 @@ function BarNavigation() {
 function AppBarTab({ children, to }: TabProps) {
   // console.log("chaging to", to);
   const { pathname } = useLocation();
-  const style =
-    pathname == to
-      ? "transition-all px-3 pt-2 text-xl font-bold capitalize text-slate-200 hover:text-yellow-400"
-      : "transition-all px-3 pt-2 text-xl font-bold capitalize text-slate-400 hover:text-green-400";
+  const style = pathname == to
+    ? "transition-all px-3 pt-2 text-xl font-bold capitalize text-slate-200 hover:text-yellow-400"
+    : "transition-all px-3 pt-2 text-xl font-bold capitalize text-slate-400 hover:text-green-400";
   return (
     <Link to={to}>
       <Text className={style}>{children}</Text>
--- src/models/routes.tsx
+++ src/models/routes.tsx
@@ -1,11 +1,57 @@
 import { FlashSpotiScreen } from "../screens/flash-list-spotify";
 import { FlatGestureScreen } from "../screens/flat-list-gesture";
 import { FlatListScreen } from "../screens/flat-list-native";
+import { GlExampleScreen } from "../screens/gl.native";
+import { GlslListScreen } from "../screens/glsl-list-native";
+import { ImgFlashScreen } from "../screens/images-flash-list";
+import { ImgGestureScreen } from "../screens/images-list-gesture";
+import { ImgListScreen } from "../screens/images-list-native";
 import { MenuScreen } from "../screens/menu-experiments";
 import { ScrollViewScreen } from "../screens/scroll-view";
 import { ScrollGestureScreen } from "../screens/scroll-view-gesture";
 
+export const IMG_LINKS = [
+  {
+    id: Math.random().toString(36).slice(2),
+    color: "peru",
+    path: "/img-flat",
+    alias: "img-flat-list",
+    screen: <ImgListScreen />,
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    color: "peru",
+    path: "/img-gesture",
+    alias: "img-gesture-list",
+    screen: <ImgGestureScreen />,
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    color: "peru",
+    path: "/img-flash",
+    alias: "img-flash-list",
+    screen: <ImgFlashScreen />,
+  },
+];
+
 export const APP_LINKS = [
+  {
+    id: Math.random().toString(36).slice(2),
+    color: "peru",
+    path: "/",
+    alias: "menu",
+    // screen: <MenuScreen />,
+    // screen: <GlExampleScreen />,
+    // screen: <FlatListScreen />,
+    screen: <ImgFlashScreen />,
+  },
+  {
+    id: Math.random().toString(36).slice(2),
+    color: "peru",
+    path: "/gl",
+    alias: "expo-gl",
+    screen: <GlExampleScreen />,
+  },
   {
     id: Math.random().toString(36).slice(2),
     color: "peru",
@@ -40,12 +86,5 @@ export const APP_LINKS = [
     path: "/scroll",
     alias: "scroll-view",
     screen: <ScrollViewScreen />,
-  },
-  {
-    id: Math.random().toString(36).slice(2),
-    color: "peru",
-    path: "/",
-    alias: "menu",
-    screen: <MenuScreen />,
   },
 ];
